6a57f58316dad82da5ea477123d46ffcce485421
Jasmin Wärnhjelm <jasmin.warnhjelm@tuni.fi>
2025-02-11 10:13:35 +0200

    Main Menu fixed

--- crankup!/Menu/MainMenu.cs
+++ crankup!/Menu/MainMenu.cs
@@ -6,8 +6,14 @@ public partial class MainMenu : Control
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		Button playButton = GetNode<Button>("PlayButton");
+		TextureButton playButton = GetNode<TextureButton>("Buttons/PlayButton");
 		playButton.Pressed += _on_play_button_pressed;
+
+		TextureButton optionsButton = GetNode<TextureButton>("Buttons/OptionsButton");
+		optionsButton.Pressed += _on_options_button_pressed;
+
+		TextureButton quitButton = GetNode<TextureButton>("Buttons/QuitButton");
+		quitButton.Pressed += _on_quit_button_pressed;
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,5 +24,18 @@ public partial class MainMenu : Control
 	{
 		GD.Print("Play Pressed");
 		GetTree().ChangeSceneToFile("res://Game/game_start.tscn");
+		GD.Print("Play scene loaded");
+	}
+	public void _on_options_button_pressed()
+	{
+		GD.Print("Options Pressed");
+		GetTree().ChangeSceneToFile("res://Option/options.tscn");
+		GD.Print("Option scene loaded");
+	}
+
+	public void _on_quit_button_pressed()
+	{
+		GD.Print("Quit Pressed");
+		GetTree().Quit();
 	}
 }
