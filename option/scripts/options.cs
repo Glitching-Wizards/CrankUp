fc10e7edb9171719740492f5e2a759b1d9d9bc53
Jasmin Wärnhjelm <jasmin.warnhjelm@tuni.fi>
2025-02-17 15:50:48 +0200

    New folder set up

---
+++ option/scripts/options.cs
@@ -0,0 +1,26 @@
+using Godot;
+using System;
+
+namespace CrankUp
+{
+	public partial class options : Control
+	{
+		// Called when the node enters the scene tree for the first time.
+		public override void _Ready()
+		{
+			//TextureButton backButton = GetNode<TextureButton>("back_button");
+			//backButton.Pressed += _on_back_button_pressed;
+		}
+
+		// Called every frame. 'delta' is the elapsed time since the previous frame.
+		public override void _Process(double delta)
+		{
+		}
+
+		public void _on_back_button_pressed()
+		{
+			//GD.Print("Back Pressed");
+			//GetTree().ChangeSceneToFile("res://menu/main_menu.tscn");
+		}
+	}
+}
