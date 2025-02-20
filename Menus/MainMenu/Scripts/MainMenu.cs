3ace9fd0a3d540fc5ce7d1d3f92dc7717107bad5
Actual <actualtw@gmail.com>
2025-02-20 18:01:30 +0200

    Working simplified main menu

--- Menus/MainMenu/Scripts/MainMenu.cs
+++ Menus/MainMenu/Scripts/MainMenu.cs
@@ -3,25 +3,21 @@ using System;
 
 namespace CrankUp
 {
-	public partial class main_menu : Control
+	public partial class MainMenu : Control
 	{
+		[Export] private string _level1ScenePath = "res://Game/Level1/Scenes/Level1.tscn";
+		[Export] private string _optionsScenePath = "res://Menus/Options/Scenes/Options.tscn";
 		private bool isSceneChanging = false;
-		// Called when the node enters the scene tree for the first time.
 		public override void _Ready()
 		{
-			TextureButton playButton = GetNode<TextureButton>("buttons/play_button");
+			TextureButton playButton = GetNode<TextureButton>("Buttons/PlayButton");
 			playButton.Pressed += _on_play_button_pressed;
 
-			TextureButton optionsButton = GetNode<TextureButton>("buttons/options_button");
+			TextureButton optionsButton = GetNode<TextureButton>("Buttons/OptionsButton");
 			optionsButton.Pressed += _on_options_button_pressed;
 
-			TextureButton quitButton = GetNode<TextureButton>("buttons/quit_button");
+			TextureButton quitButton = GetNode<TextureButton>("Buttons/QuitButton");
 			quitButton.Pressed += _on_quit_button_pressed;
-		}
-
-		// Called every frame. 'delta' is the elapsed time since the previous frame.
-		public override void _Process(double delta)
-		{
 		}
 
 		public void _on_play_button_pressed()
@@ -38,7 +34,7 @@ namespace CrankUp
             var tree = GetTree();
             if (tree != null)
             {
-                tree.ChangeSceneToFile("res://game/scenes/game_start.tscn"); //
+                tree.ChangeSceneToFile(_level1ScenePath);
                 GD.Print("Play scene loaded");
             }
             else
@@ -60,7 +56,7 @@ namespace CrankUp
 			var tree = GetTree();
             if (tree != null)
             {
-                tree.ChangeSceneToFile("res://option/scenes/options.tscn");
+                tree.ChangeSceneToFile(_optionsScenePath);
                 GD.Print("Option scene loaded");
             }
             else
