7e4ba7bd26df88e7fcfe428a460ee304ee8c81f5
Juho Ström <juho.strom@tuni.fi>
2025-03-24 11:52:31 +0200

    Added Level2, 3, 4 and 5 scenes. Added start level windows for each level. Updated the script to correctly open the chosen level

---
+++ Game/Level2/Scripts/Level2.cs
@@ -0,0 +1,146 @@
+using Godot;
+using System;
+using System.Numerics;
+using System.Runtime.CompilerServices;
+
+namespace CrankUp;
+public partial class Level2 : Node2D
+{
+	private static Level2 _current = null;
+	public static Level2 Current
+	{
+		get { return _current; }
+	}
+
+	[Export] private string _clawScenePath = "res://Game/Scenes/Claw.tscn";
+	[Export] private string _settingsScenePath = "res://Menus/Settings/Scenes/Settings.tscn";
+
+	[Export] private string _blockScenePath = "res://Game/Scenes/Block.tscn";
+	[Export] private string _longBlockScenePath = "res://Game/Scenes/LongBlock.tscn";
+
+	private Window settingsWindow;
+	private PackedScene _clawScene = null;
+	private Claw _claw = null;
+	public Claw Claw => _claw;
+	private ClawHead clawHead;
+
+	private TextureRect conveyorBelt;
+	private PackedScene _blockScene;
+	private PackedScene _longBlockScene;
+	private TextureButton blockButton;
+	private TextureButton longBlockButton;
+	private bool longBlockButtonPressed = false;
+	private bool blockButtonPressed = false;
+
+	private bool startLevel = true;
+	private bool endLevel = false;
+	private float beltTargetPositionStart = -540;
+	private float beltTargetPositionEnd = 460; // conveyorBelt target position when all blocks are in play
+	private float beltMoveSpeed = 200f; // conveyorBelts speed of movement per frame
+
+
+	public override void _Ready()
+	{
+		_claw = CreateClaw();
+
+		clawHead = GetTree().Root.FindChild("ClawHead", true, false) as ClawHead;
+		if (clawHead == null)
+		{
+			GD.PrintErr("[ERROR] ClawHead not found! Block cannot be spawned.");
+			return;
+		}
+
+		conveyorBelt = GetNodeOrNull<TextureRect>("ConveyorBelt");
+
+		PackedScene settingsScene = (PackedScene)GD.Load(_settingsScenePath);
+		settingsWindow = (Window)settingsScene.Instantiate();
+		AddChild(settingsWindow);
+		settingsWindow.Hide();
+
+		_blockScene = ResourceLoader.Load<PackedScene>(_blockScenePath);
+		_longBlockScene = ResourceLoader.Load<PackedScene>(_longBlockScenePath);
+
+		blockButton = GetNodeOrNull<TextureButton>("ConveyorBelt/BlockButtons/Block");
+		longBlockButton = GetNodeOrNull<TextureButton>("ConveyorBelt/BlockButtons/LongBlock");
+
+		blockButton.Pressed += () =>
+		{
+			blockButtonPressed = true;
+			SpawnBlockButtonPressed(_blockScene, blockButton);
+		};
+
+		longBlockButton.Pressed += () =>
+		{
+			longBlockButtonPressed = true;
+			SpawnBlockButtonPressed(_longBlockScene, longBlockButton);
+		};
+	}
+
+	public Level2()
+	{
+		_current = this;
+	}
+
+	private Claw CreateClaw()
+	{
+		if (_clawScene == null)
+		{
+			_clawScene = ResourceLoader.Load<PackedScene>(_clawScenePath);
+			if (_clawScene == null) // Check if the scene is loaded correctly
+			{
+				GD.PrintErr("Claw scene not found!");
+				return null;
+			}
+		}
+		return _clawScene.Instantiate<Claw>(); // Instantiate Claw
+	}
+
+	private async void SpawnBlockButtonPressed(PackedScene BlockScene, TextureButton button)
+	{
+		if (clawHead.grabbedBlock != null) return;
+
+		if (BlockScene == null)
+		{
+			GD.PrintErr("[ERROR] Cannot spawn block, scene not loaded!");
+		}
+
+		clawHead.collisionShape.SetDeferred("disabled", true);
+
+		RigidBody2D blockInstance = BlockScene.Instantiate<RigidBody2D>();
+		this.AddChild(blockInstance);
+
+		blockInstance.GlobalPosition = clawHead.GlobalPosition + new Godot.Vector2(0, 20);
+
+		await ToSignal(GetTree().CreateTimer(0.1f), "timeout");
+
+		clawHead.GrabBlock();
+
+		button.QueueFree();
+
+		// Check if all buttons are removed
+		if (blockButtonPressed && longBlockButtonPressed) endLevel = true;
+	}
+
+	public override void _Process(double delta)
+	{
+		if (conveyorBelt != null && startLevel)
+		{
+			float step = beltMoveSpeed * (float) delta;
+
+			if (conveyorBelt.Position.X < beltTargetPositionStart)
+			{
+				conveyorBelt.Position = new Godot.Vector2(conveyorBelt.Position.X + step, conveyorBelt.Position.Y);
+			}
+		}
+
+		if (conveyorBelt != null && endLevel)
+		{
+			float step = beltMoveSpeed * (float) delta;
+
+			if (conveyorBelt.Position.X < beltTargetPositionEnd)
+			{
+				conveyorBelt.Position = new Godot.Vector2(conveyorBelt.Position.X + step, conveyorBelt.Position.Y);
+			}
+		}
+	}
+}
