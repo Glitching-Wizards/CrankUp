922a4c920661e9ee970ef6d55708a13fbe3f6c1c
Juho Ström <juho.strom@tuni.fi>
2025-04-15 10:33:11 +0300

    Claw now moves to the top of the screen when fetching new block from the conveyor belt. Changed ordering of claw so that it is on top of the placement area

--- Game/Level3/Scripts/Level3.cs
+++ Game/Level3/Scripts/Level3.cs
@@ -207,6 +207,8 @@ public partial class Level3 : Node2D
 			GD.PrintErr("[ERROR] Cannot spawn block, scene not loaded!");
 		}
 
+		clawHead.GlobalPosition = new Godot.Vector2 (clawHead.GlobalPosition.X, -291);
+
 		clawHead.collisionShape.SetDeferred("disabled", true);
 
 		RigidBody2D blockInstance = BlockScene.Instantiate<RigidBody2D>();
